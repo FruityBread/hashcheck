2eb6e446792aa32891ef3837eb606fba0e79e96d
Alex Armes <45469986+FruityBread@users.noreply.github.com>
2022-02-06 10:33:02 +0000

    can choose hash type

--- main.py
+++ main.py
@@ -3,16 +3,38 @@ import hashlib
 
 BUF_SIZE = 65536 
 
-md5 = hashlib.md5()
-sha1 = hashlib.sha256()
+def hashselect():
+    print("(1) SHA256 \n(2) SHA512 \n(3) MD5")
+    choice=input("")
+    if choice =="1":
+        type = ("SHA256")
+        hash = hashlib.sha256()
+        openfile(hash)
+        output(type, hash)
+    elif choice =="2":
+        type = ("SHA512")
+        hash = hashlib.sha512()
+        openfile(hash)
+        output(type, hash)
+    elif choice=="3":
+        type = ("MD5")
+        hash = hashlib.md5()
+        openfile(hash)
+        output(type, hash)
+    else:
+        print("Please enter a valid option.")
+        hashselect()
 
-with open((sys.argv[1]), 'rb') as f:
-    while True:
-        data = f.read(BUF_SIZE)
-        if not data:
-            break
-        md5.update(data)
-        sha1.update(data)
 
-print("MD5: {0}".format(md5.hexdigest()))
-print("SHA1: {0}".format(sha1.hexdigest()))
+def openfile(hash):
+    with open((sys.argv[1]), 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            hash.update(data)
+
+def output(type, hash):   
+    print(type+": {0}".format(hash.hexdigest()))
+
+hashselect()
