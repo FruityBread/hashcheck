84f050e239923f0505f5bf2050ee0ce5ecf493cd
Alex Armes <45469986+FruityBread@users.noreply.github.com>
2022-02-08 11:05:00 +0000

    progress to gui

--- functions.py
+++ functions.py
@@ -4,10 +4,6 @@ import tkinter as tk
 from tkinter.filedialog import askopenfilename
 from tkinter import ttk
 
-
-root = tk.Tk()
-root.withdraw()
-
 BUF_SIZE = 65536 
 
 '''def hashselect():
@@ -33,28 +29,32 @@ BUF_SIZE = 65536
         print("Please enter a valid option.")
         hashselect()'''
 
-def hashchoice(choice):
+def hashchoice(choice, file):
     if choice =="1":
         type = ("SHA256")
         hash = hashlib.sha256()
-        openfile(hash)
-        output(type, hash)
+        #output(type, hash)
     elif choice =="2":
         type = ("SHA512")
         hash = hashlib.sha512()
-        openfile(hash)
-        output(type, hash)
+        #output(type, hash)
     elif choice=="3":
         type = ("MD5")
         hash = hashlib.md5()
-        openfile(hash)
-        output(type, hash)
+        #output(type, hash)
     else:
         print("Please enter a valid option.")
         hashchoice()
 
-
-def openfile(hash):
+    with open((file), 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                return hash
+                #root.destroy()
+                break
+            hash.update(data)
+'''def openfile(hash):
     file = askopenfilename()
     print(file)
     with open((file), 'rb') as f:
@@ -63,13 +63,17 @@ def openfile(hash):
             if not data:
                 #root.destroy()
                 break
-            hash.update(data)
+            hash.update(data)'''
 
-def output(type, hash):   
+def outputCheck(submission, hash):
+    print(hash.hexdigest())
+
+
+'''def output(type, hash):   
     print(type+": {0}".format(hash.hexdigest()))
     check=input("\nEnter hash value to check against\n")
     print(hash.hexdigest())
     if hash.hexdigest()==check:
         print("They match")
     else:
-        print("They do not match")
+        print("They do not match")'''
